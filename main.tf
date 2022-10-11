7acfea3f26ca9eb7c61461ae119afa466e88eb55
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-11 16:43:18 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "null_resource" "example2" {
   provisioner "local-exec" {
-    command = "Get-Date > completed.txt"
-    interpreter = ["PowerShell", "-Command"]
+    command = "echo rm"
   }
 }
 
