87ee2fd1ab5b1be3e04113dcc82185010c1fb85d
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-11 16:43:47 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "null_resource" "example2" {
 #     command = "echo test2"
 #   }
   
-}
+# }
 # module "rm" {
 # 	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 # 	version = "2.0.2"
