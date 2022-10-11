7b29f4f0e975d54a937722b25e803ba13aa8cc78
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-11 16:49:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
+
 resource "null_resource" "example2" {
   provisioner "local-exec" {
     command = "echo rm"
