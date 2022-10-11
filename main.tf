3818ad07e2c9518c9396ec215f50928cb7b19a97
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-11 12:43:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
+}
 module "rm" {
 	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 	version = "2.0.2"
