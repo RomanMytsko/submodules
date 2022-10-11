004fb303864a84be7b9f05da735349b29d595cf3
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-11 16:38:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 2.43"
+    }
+    null = {
+        version = "~> 3.0.0"
+    }
+}
+
 resource "null_resource" "name" {
   name = "test"
   provisioner "local-exec" {
