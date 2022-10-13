cd613d668d04363296eed03ef2ea6e5b1ee17620
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-13 17:01:11 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,10 @@
-# terraform {
-#   required_providers {
-#     null = {
-#       source = "hashicorp/null"
-#       version = "3.1.1"
-#     }
-#   }
-# }
-
-# provider "null" {
-  # Configuration options
-# }
-
-# resource "null_resource" "example2" {
-#   provisioner "local-exec" {
-#     command = "echo rm"
-#   }
-# }
-
-# resource "null_resource" "name" {
-#   name = "test"
-#   provisioner "local-exec" {
-#     command = "echo test2"
-#   }
-  
-# }
 # module "rm" {
 # 	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 # 	version = "2.0.2"
 # }
 	
-module "rm2" {
-	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
-	version = "1.0.0"
-	counts = 2
-}
-
-# variable "counts" {
-#   default = 2
+# module "rm2" {
+# 	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
+# 	version = "1.0.0"
+# 	counts = 2
 # }
