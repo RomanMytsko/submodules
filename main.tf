2e942ed03d32889d1172a3006e79e8660c3ec4fb
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-10-26 16:56:22 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "rm" {
-	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
+	source  = "0cc28c0a208c.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 	version = "2.0.2"
 }
 	
-module "rm2" {
-	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
-	version = "1.0.0"
-	counts = 2
-}
+# module "rm2" {
+# 	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
+# 	version = "1.0.0"
+# 	counts = 2
+# }
