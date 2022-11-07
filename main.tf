ae3303eda34b7e89969e813638c941fd74d3d5c6
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-11-07 15:58:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,12 +22,11 @@ provider "google" {
 }
 
 module "rm" {
-	source  = "0cc28c0a208c.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
+	source  = "f6e71f23d20d.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 	version = "2.0.2"
 }
 	
-module "rm2" {
-	source  = "036a940647f4.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
-	version = "1.0.0"
-	counts = 2
-}
+# module "rm2" {
+# 	source  = "f6e71f23d20d.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
+# 	version = "1.0.0"
+# }
