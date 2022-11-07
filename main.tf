cc315b4a6f6d9960725e8e18601ed1a5f5215fd4
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-11-07 15:24:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.24.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = ">=3.24.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "google" {
+  region = "us-east1"
+}
+
 module "rm" {
 	source  = "0cc28c0a208c.test-env.scalr.com/env-svrcnchebt61e30/rm/local"
 	version = "2.0.2"
