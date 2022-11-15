9019c44c2af581d2da1d40929748a700da20095f
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-11-15 11:03:29 +0200

    Update main.tf

--- terraform-scalr-local/main.tf
+++ terraform-scalr-local/main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.24.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = ">=3.24.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "google" {
+  region = "us-east1"
+}
+
 resource "null_resource" "name" {
   name = "test"
   provisioner "local-exec" {
