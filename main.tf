215ef38e349940445a7c965bc97856a27ad25ef2
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-11-24 16:35:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=3.24.0"
-    }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">=3.24.0"
+#     }
 
-    google = {
-      source  = "hashicorp/google"
-      version = ">=3.24.0"
-    }
-  }
+#     google = {
+#       source  = "hashicorp/google"
+#       version = ">=3.24.0"
+#     }
+#   }
 
-}
+# }
 
 module "rm" {
 source = "git@github.com:RomanMytsko/local-exec"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-provider "google" {
-  region = "us-east1"
-}
+# provider "google" {
+#   region = "us-east1"
+# }
 
 # module "rm" {
 # 	source  = "13bcb3bc2462.test-env.scalr.com/acc-svrcncgh453bi8g/rm/local"
