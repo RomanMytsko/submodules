3422beb3ee703e73d8ad6a8d195a30998837181a
Roman <37997220+RomanMytsko@users.noreply.github.com>
2022-12-20 11:29:14 +0200

    Update main.tf

--- subdir/main.tf
+++ subdir/main.tf
@@ -2,3 +2,8 @@ module "rm" {
 	source  = "mainiacp.org10.te.scalr-labs.net/acc-svrcncgh453bi8g/rm/local"
 	version = "3.0.1"
 }
+	
+module "rm2" {
+	source  = "mainiacp.org10.te.scalr-labs.net/env-svrcnchebt61e30/rm2/local"
+	version = "1.0.0"
+	}
