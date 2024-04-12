dcbe07e37e374d0c316f8220ff83c14a07f58cfb
roman <r.mytsko@scalr.com>
2024-04-12 09:22:54 +0300

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-module "instance" {
-  source = "./subdir"
+resource "null_resource" "example" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "This is TF SUBMODULE"
+  }
 }
